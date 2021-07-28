0de67e1836568cda6982ddee18441db932b68206
Yuriko Maruyama <yuriko-maruyama@hiroshima-u.ac.jp>
2021-07-28 01:13:39 +0000

    sprint3

--- janken.go
+++ janken.go
@@ -1,4 +1,4 @@
-package janken
+package main
 
 import (
 	"fmt"
@@ -6,9 +6,22 @@ import (
 	"time"
 )
 
-func get_cpu() int {
+func random_cpu() int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(3)
+}
+
+func print_cpu(input int) {
+	switch input {
+	case 0:
+		fmt.Println("The CPU's rock-paper-scissors hand is r")
+	case 1:
+		fmt.Println("The CPU's rock-paper-scissors hand is s")
+	case 2:
+		fmt.Println("The CPU's rock-paper-scissors hand is p")
+	default:
+		// nothing
+	}
 }
 
 func judge_win_lose(user int, cpu int) int {
@@ -36,49 +49,6 @@ func judge_win_lose(user int, cpu int) int {
 	}
 }
 
-func janken_loop() {
-	var user int
-	var cpu int
-	var userwin int
-	var cpuwin int
-	var draw int
-
-	user = 0 //strで受け取ってintに直す関数
-	cpu = 1  //いったん決め打ち
-	for {
-		tmp = judge_win_lose(user, cpu)
-		switch tmp {
-		case 0:
-			userwin += 1
-		case 1:
-			cpuwin += 1
-		case 2:
-			draw += 1
-		}
-		fmt.Printf("User win:%d lose:%d draw:%d\n", userwin, cpuwin, draw)
-	}
-
-func judge_win_lose(user int, cpu int) int {
-	if user == cpu { //あいこ
-		return 2
-	} else if user == 0 && cpu == 1 { // user: グー, cpu:チョキ
-		return 0
-	} else if user == 0 && cpu == 2 { // user: グー, cpu:パー
-		return 1
-	} else if user == 1 && cpu == 0 { // user: チョキ, cpu:グー
-		return 1
-	} else if user == 1 && cpu == 2 { // user: チョキ, cpu:パー
-		return 0
-	} else if user == 2 && cpu == 0 { // user: パー, cpu:グー
-		return 0
-	} else if user == 2 && cpu == 1 { // user: パー, cpu:チョキ
-		return 1
-	} else {
-		return 2
-	}
-}
-
-
 func janken_loop() (int, int, int) {
 	var user int
 	var cpu int
@@ -86,17 +56,21 @@ func janken_loop() (int, int, int) {
 	var cpuwin int
 	var draw int
 	var suser string
-	var result int
+	// var result int
 
 	for {
+		fmt.Println("Type \"exit\" if you want to quit.")
+		fmt.Println("Please enter the user's hand")
+
 		fmt.Scanf("%s", &suser)
 		if suser == "exit" {
 			break
 		}
 		user = get_user(suser) //strで受け取ってintに直す関数
-		cpu = get_cpu()        //ランダム生成関数
+		cpu = random_cpu()     //ランダム生成関数
+		print_cpu(cpu)
 
-		tmp = judge_win_lose(user, cpu)
+		tmp := judge_win_lose(user, cpu)
 		switch tmp {
 		case 0:
 			userwin += 1
@@ -107,10 +81,13 @@ func janken_loop() (int, int, int) {
 		case 2:
 			draw += 1
 			fmt.Println("Draw")
+		default:
+			// nothing
 		}
+		fmt.Println("----------------------------------------------")
 	}
 
-	result = judge(userwin, cpuwin, draw)
+	_ = judge(userwin, cpuwin, draw)
 
 	return userwin, cpuwin, draw
 }
@@ -125,33 +102,35 @@ func get_user(input string) int {
 		input_int = 1
 	case "p":
 		input_int = 2
+	default:
+		// fmt.Println("input correct hand)
 	}
 
 	return input_int
 }
 
 func judge(userwin int, cpuwin int, draw int) int {
+	// testのために誰が勝ったかを返す
 	var win_lose int
 
 	if userwin > cpuwin {
 		fmt.Println("User win")
-		fmt.Printf("win:%d lose:%d draw:%d\n", userwin, cpuwin, draw)
+		fmt.Printf("win:%d, lose:%d, draw:%d\n", userwin, cpuwin, draw)
 		win_lose = 0
 	} else if cpuwin > userwin {
 		fmt.Println("Cpu win")
-		fmt.Printf("win:%d lose:%d draw:%d\n", userwin, cpuwin, draw)
+		fmt.Printf("win:%d, lose:%d, draw:%d\n", userwin, cpuwin, draw)
 		win_lose = 1
 	} else {
 		fmt.Println("Draw")
-		fmt.Printf("user_win:%d cpu_win:%d draw:%d\n", userwin, cpuwin, draw)
+		fmt.Printf("user_win:%d, cpu_win:%d, draw:%d\n", userwin, cpuwin, draw)
 		win_lose = 2
 	}
-
 	return win_lose
 }
 
 func main() {
-	c := get_cpu()
+	c := random_cpu()
 	fmt.Println(c)
 	fmt.Println("user input: rock = r, scissors = s, paper = p")
 	janken_loop()
